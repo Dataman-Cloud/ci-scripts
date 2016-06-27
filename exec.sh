2d7316581835999260980459d139e14b4061af91
yp2800 <yp2800@gmail.com>
2016-06-27 11:19:01 +0800

    update

--- exec.sh
+++ exec.sh
@@ -234,7 +234,7 @@ generate_image_name(){
 code_compile() {
     if [ -f "$SERVICE/deploy/compile.sh" ]; then
         . $SERVICE/deploy/compile.sh
-        docker run --rm -e SERVICE="$SERVICE" -e GO15VENDOREXPERIMENT=1 -e GOPATH="/usr/local/go" -v /tmp/codebuild:/data/build -w="/data/build" $code_compile_image /bin/bash -c "bash -x compile.sh"
+        docker run --rm -e SERVICE="$SERVICE" -v /tmp/codebuild:/data/build -w="/data/build" $code_compile_image /bin/bash -c "bash -x compile.sh"
     fi
 	[ $? -eq 0 ] || error "build $SERVICE failed."
 }
