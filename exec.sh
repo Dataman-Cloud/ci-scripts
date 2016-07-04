2cf93b05dda7492f11d265f9f8ee42f9d024b147
yp2800 <yp2800@gmail.com>
2016-07-04 19:13:51 +0800

    update

--- exec.sh
+++ exec.sh
@@ -246,7 +246,7 @@ code_compile() {
     fi
     [ $? -eq 0 ] || error "build $SERVICE failed."
     if [ $SERVICE = "webpage" ] || [ $SERVICE = "frontend" ];then
-        docker run --rm -d -v /tmp/codebuild/$SERVICE:/usr/src/myapp -w /usr/src/myapp demoregistry.dataman-inc.com/library/node-gulp:v0.1.063000 /bin/bash compress.sh
+        docker run -d --name compress-"$SERVICE" -v /tmp/codebuild/$SERVICE:/usr/src/myapp -w /usr/src/myapp demoregistry.dataman-inc.com/library/node-gulp:v0.1.063000 /bin/bash compress.sh
     fi
     if [ $? -eq 0 ]; then
         tar -zcvf $SERVICE.tar.gz $SERVICE	  
